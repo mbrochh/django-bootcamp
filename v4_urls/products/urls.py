bdbb315231ac9971b1fea88807e53dc20e48a17e
Martin Brochhaus <mbrochh@gmail.com>
2016-12-15 18:16:04 +0800

    Deleted all files so that repo can be cloned directly into c9 workspace

--- v4_urls/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    url(r'^(?P<pk>\d+)/(?P<slug>[-_\w]+)/',
-        TemplateView.as_view(template_name='products/products_detail_view.html'),
-        name='products_detail'),
-]
