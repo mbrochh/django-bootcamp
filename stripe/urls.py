f04c1fd6c8b16538a1162fea1840a3aac1f7f196
Martin Brochhaus <mbrochh@gmail.com>
2016-12-15 18:37:48 +0800

    Prepared files for chapter v4_urls

---
+++ stripe/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
+urlpatterns = [
+]
--- v7_forms/user_profiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    url(r'^shipping/',
-        TemplateView.as_view(
-            template_name='user_profiles/shipping_address_view.html'),
-        name='user_profiles_shipping'),
-]
